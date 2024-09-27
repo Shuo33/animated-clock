043b4367a7f78d3d98767a2397c0c09281151e2c
shuo xu <shuo.xu@2021.icam.fr>
2024-09-27 14:31:45 +0200

    add functions: color changing & download

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+const canvas = document.getElementById('canvas');
+
+const faceColor = document.getElementById('face-color');
+const borderColor = document.getElementById('border-color');
+const numberLinesColor = document.getElementById('line-color');
+const largeHandsColor = document.getElementById('large-hand-color');
+const secondHandeColor = document.getElementById('second-hand-color');
+
 function clock() {
     const now = new Date();
-    console.log(now);
+    // console.log(now);
 
-    const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
 
     // Setup canvas
@@ -21,7 +28,8 @@ function clock() {
     ctx.save();
     ctx.beginPath();
     ctx.lineWidth = 14;
-    ctx.strokeStyle = '#800000';
+    ctx.strokeStyle = borderColor.value;
+    ctx.fillStyle = faceColor.value;
     ctx.arc(0, 0, 142, 0, Math.PI * 2, true);
     ctx.stroke();
     ctx.fill();
@@ -29,11 +37,12 @@ function clock() {
 
     // Draw hour lines
     ctx.save();
+    ctx.strokeStyle = numberLinesColor.value; 
     for (let i = 0; i < 12; i++){
         ctx.beginPath();
+        ctx.rotate(Math.PI / 6); 
         ctx.moveTo(100, 0);
         ctx.lineTo(120, 0);
-        ctx.rotate(Math.PI / 6); 
         ctx.stroke();
     }
     ctx.restore();
@@ -41,6 +50,7 @@ function clock() {
     // Draw minute lines 
     ctx.save();
     ctx.lineWidth = 4;
+    ctx.strokeStyle = numberLinesColor.value; 
     for (let i = 0; i < 60; i++){
         if (i % 5 !== 0) {
             ctx.beginPath();
@@ -56,7 +66,7 @@ function clock() {
     const hr = now.getHours() % 12; 
     const min = now.getMinutes();
     const sec = now.getSeconds();
-    console.log(`${hr}:${min}:${sec}`);
+    // console.log(`${hr}:${min}:${sec}`);
 
     ctx.strokeStyle = '#8000000';
 
@@ -65,8 +75,9 @@ function clock() {
     ctx.save();
     ctx.rotate((Math.PI / 6) * hr + ((Math.PI / 6) / 60) * min + ((Math.PI / 6) / 3600) * sec);
     // Math.PI / 6hr = 180deg / 6hr = 30deg per hr
-    // Math.PI / 6hr => 30deg per hr, 30deg per hr / 60mins = 0.5deg per mins 
+    // Math.PI / 6hr => 30deg per hr, 30deg per hr / 60mins = 0.5deg per mins
     // Math.PI / 6hr => 30deg per hr, 30deg per hr / 3600secs = 0.0083deg per second
+    ctx.strokeStyle = largeHandsColor.value;
     ctx.lineWidth = 14;
     ctx.beginPath();
     ctx.moveTo(-20, 0);
@@ -92,8 +103,8 @@ function clock() {
     ctx.save();
     ctx.rotate((Math.PI / 30) * sec);
     // Math.PI / 30 = 180deg / 30secs = 6deg per second 
-    ctx.strokeStyle = '#FF7F50';
-    ctx.fillStyle = '#FF7F50';
+    ctx.strokeStyle = secondHandeColor.value;
+    ctx.fillStyle = secondHandeColor.value;
     ctx.lineWidth = 6;
     ctx.beginPath();
     ctx.moveTo(-30, 0);
@@ -110,6 +121,14 @@ function clock() {
 }
 
 requestAnimationFrame(clock);
+
+document.getElementById('save-btn').addEventListener('click', function () {
+    const dataURL = canvas.toDataURL('image/png'); // get the data/image URL put it into dataURL
+    const link = document.createElement('a'); // create <a></a>  
+    link.href = dataURL; // <a href = ... , which is the dataURL></a>
+    link.download = 'clock.png'; //naming the data/image 
+    link.click(); //once we click the 'save-btn' = we click the link = we fire off the download
+});
 
 
 
